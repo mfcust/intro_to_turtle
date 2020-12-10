ca4bd78b1cacb2262da772f6f6f51412d8c95d7e
mfcust <43455391+mfcust@users.noreply.github.com>
2020-12-09 17:16:04 -0800

    Update intro_to_turtle.py

--- intro_to_turtle.py
+++ intro_to_turtle.py
@@ -10,76 +10,76 @@ t = turtle.Pen()
 
 
 
-#Move your turtle forward by 100 pixels using t.forward(100)
+#1) Move your turtle forward by 100 pixels using t.forward(100)
 
 
 
 
-#You can set the speed at which your turtle travels by typing t.speed(integer between 1 and 10). Do that below:
+#2) You can set the speed at which your turtle travels by typing t.speed(integer between 1 and 10). Do that below:
 
 
 
 
-#Have your turtle move backwards by 100 pixels by using t.backward(100)
-
-
-
-
-
-#Have your turtle turn left or right a full 360 degrees (Hint: It will look similar to the two commands you just made with forward and backward)
+#3) Have your turtle move backwards by 100 pixels by using t.backward(100)
 
 
 
 
 
-#Draw a square with your turtle
+#4) Have your turtle turn left or right a full 360 degrees (Hint: It will look similar to the two commands you just made with forward and backward)
 
 
 
 
-#You can also set the color of the pen by using the following code: t.pencolor("color")
+
+#5) Draw a square with your turtle
+
+
+
+
+#6) You can also set the color of the pen by using the following code: t.pencolor("color")
 #Draw a triangle with your turtle, where each side of the triangle is a different color
 
 
 
 
 
-#Typing out t.up() lifts the pen off the canvas so you are no longer drawing. t.down() puts the pen back down on the canvas so you can continue drawing.
+#7) Typing out t.up() lifts the pen off the canvas so you are no longer drawing. t.down() puts the pen back down on the canvas so you can continue drawing.
 #Try lifting the pen up, moving your turtle to a different location, and then putting the pen back down and drawing a straight line.
 
 
 
 
 
-#t.clear() resets the canvas but leaves the turtle where you left it. t.reset() resets the entire canvas and moves the turtle back to the starting position.
+#8) t.clear() resets the canvas but leaves the turtle where you left it. t.reset() resets the entire canvas and moves the turtle back to the starting position.
 #Try doing one of these now on the line below:
 
 
 
 
 
-#Using what you know, draw two parallel lines using the turtle. Make sure you lift the pen up and put it back down when you do this so the lines aren't touching
+#9) Using what you know, draw two parallel lines using the turtle. Make sure you lift the pen up and put it back down when you do this so the lines aren't touching
 
 
 
 
-#You can draw a circle using t.circle(radius, arc, steps). Use all three arguments and try to determine what each does:
+#10) You can draw a circle using t.circle(radius, arc, steps). Use all three arguments and try to determine what each does:
 
 
 
 
 
 #Bonus Challenges:
-#Draw a square where the corners are missing
+#1) Draw a square where the corners are missing
 
 
 
 
-#Draw a spiral with your turtle. Hint: This is made easier by using a for loop!
+#2) Draw a spiral with your turtle. Hint: This is made easier by using a for loop!
 
 
 
 
 
-#Hard: If you were able to complete the previous challenge, then try to change it so you each time you draw a line, you change your pen color.
+#3) Hard: If you were able to complete the previous challenge, then try to change it so you each time you draw a line, you change your pen color.
 #Hint: You will need a list, a for loop, and you must is the modulo operator!
